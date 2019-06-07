e8d88a97113e3a89be0ffcc73c05a35f688233ba
Devin Guerrero <devinguerrero1015@gmail.com>
2019-06-07 13:00:37 -0400

    okay

--- chuck.rb
+++ chuck.rb
@@ -1,10 +1,11 @@
 class ChuckJokes
+  require 'chuck_norris'
   def help
 
   end
 
   def get_joke_by_id(id)
-
+     ChuckNorris::JokeFinder.find_joke(id)
   end
 
   def replace_name(name)
@@ -12,6 +13,6 @@ class ChuckJokes
   end
 
   def random
-
+ joke = ChuckNorris::JokeFinder.get_joke
   end
 end
